c4b7d0c5bd48a63a0c0dda33fae76139ff8521f0
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-12-06 22:26:20 -0500

    Improving style and functionality of inline edit

--- src/components/InlineEdit.js
+++ src/components/InlineEdit.js
@@ -25,6 +25,12 @@ export default class InlineEdit extends React.Component {
         // Set text back to original
         this.setState({ editing: this.state.original, isEditing: false });
       }
+      else {
+        this.setState({ original: this.state.editing });
+      }
+    }
+    else {
+      this.setState({ original: this.state.editing });
     }
   }
 
@@ -39,24 +45,37 @@ export default class InlineEdit extends React.Component {
     }
   }
 
+  handleOnFocus = (e) => {
+    const val = e.target.value;
+    e.target.value = "";
+    e.target.value = val;
+  }
+
   render() {
     return (
       <div
-        className="inline-edit-wrap"
+        className={this.state.isEditing
+          ? `inline-edit-wrap-editing ${this.props.wrapperClassName? this.props.wrapperClassName: ""}`
+          : `inline-edit-wrap ${this.props.wrapperClassName? this.props.wrapperClassName: ""}`
+        }
         onClick={()=>this.setState({ isEditing: true })}
       >
         {this.state.isEditing
           ? <TextArea
               rows={1}
-              className="inline-edit"
+              className={this.state.isEditing
+                ? `inline-edit-editing ${this.props.textAreaClassName? this.props.textAreaClassName : ""}`
+                : `inline-edit ${this.props.textAreaClassName? this.props.textAreaClassName : ""}`
+              }
               onChange={(e)=>this.setState({ editing: e.target.value })}
               onBlur={this.handleOnBlur}
+              onFocus={this.handleOnFocus}
               value={this.state.editing}
               onKeyDown={this.handleKeyDown}
               autoFocus
             />
           : <span
-              className="inline-edit"
+              className={`inline-edit ${this.props.textClassName? this.props.textClassName : ""}`}
             >
               {this.state.editing}
             </span>
